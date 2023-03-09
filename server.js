5337439f83fd3629d449a189b3992bcb5b530a2e
Rayane Benamre <thegamercore3@gmail.com>
2023-03-09 11:44:06 +0100

    fix: remove default port fallback in server.js file

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log('Listening on port ' + PORT + '!');
 });
