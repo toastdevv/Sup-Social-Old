9c27133b061e435603fa6ed1b68ef556dcf402ac
Rayane Benamre <thegamercore3@gmail.com>
2023-03-14 12:51:02 +0100

    MVP CC page done, rooms beginning of development

---
+++ public/scripts/room.js
@@ -0,0 +1,60 @@
+let messageForm = document.getElementById('message-form');
+let messageField = document.getElementById('message-field');
+
+let messagesContainer = document.getElementById('messages-container');
+
+let locTemp = window.location.href.split('/');
+
+let dmName = locTemp[locTemp.length - 1];
+
+console.log(dmName);
+
+/*global io*/
+const socket = io();
+
+socket.emit('room join', { dmName });
+
+function addMessage(message) {
+	let messageDiv = document.createElement('div');
+	messageDiv.classList.add('message');
+
+	let messageText = document.createElement('p');
+	messageText.style.fontSize = '1.3em';
+	messageText.innerText = message.username + ': ' + message.message;
+
+	messageDiv.appendChild(messageText);
+
+	messagesContainer.appendChild(messageDiv);
+
+	messagesContainer.scrollTop = messagesContainer.scrollHeight;
+}
+
+document.addEventListener('DOMContentLoaded', async e => {
+	let messagesReq = await fetch('/messages/get/' + dmName, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+
+	let messages = await messagesReq.json();
+
+	messages.forEach(message => {
+		addMessage(message);
+	})
+});
+
+messageForm.addEventListener('submit', async e => {
+	e.preventDefault();
+
+	let message = '' + messageField.value;
+
+	messageField.value = '';
+
+
+	socket.emit('dm message', { message: message, dmName: dmName });
+});
+
+socket.on('dm message', message => {
+	addMessage(message);
+});
